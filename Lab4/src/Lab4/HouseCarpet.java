08c874edd352b08169e043b2bb60203c53cdef78
accehikllmr <135480854+accehikllmr@users.noreply.github.com>
2025-03-04 18:49:24 -0500

    OOP challenge
    finished first two challenges, working on third one, more complex as it calls methods from previous two classes and requires working with arrays, will come back to it later this week

--- Lab4/src/Lab4/HouseCarpet.java
+++ Lab4/src/Lab4/HouseCarpet.java
@@ -1,4 +1,59 @@
 package Lab4;
 
 public class HouseCarpet {
+	
+	// double check how an Array object can be initialized
+	private RoomDimension [] rooms;
+	private double cost;
+	
+	// general class constructor
+	public HouseCarpet(int c, double cost) {
+		// validating argument passed to c parameter, should not be a negative value, default to 0 rooms
+		this.rooms = (c >= 0) ? new RoomDimension[c] : new RoomDimension[0];
+		// validating argument passed to cost parameter, should not be a negative value, default to cost of 0
+		this.cost = (cost >= 0) ? cost : 0;
+	}
+	
+	// accessor methods
+	public RoomDimension [] getRooms() {
+		return this.rooms;
+	}
+	
+	public double getCost( ) {
+		return this.cost;
+	}
+	
+	// mutator methods
+	public void setCost(double cost) {
+		this.cost = (cost >= 0) ? cost : 0;
+	}
+	
+	public void setRooms(RoomDimension rooms) {
+		
+	}
+	
+	// computes the cost of carpeting for each individual room, and stores these costs in an array
+	public double [] getRoomCosts() {
+		// initializing new Array object in which to store costs, length is equivalent to number of rooms in RoomDimension object
+		double [] roomCosts = new double[this.rooms.length];
+		// iterating through all RoomDimension objects to retrieve their area
+		for (int i = 0; i < this.rooms.length; i++) {
+			// retrieving area from each individual RoomDimension object, and multiplying it by cost of carpet
+			roomCosts[i] = this.rooms[i].getArea() * this.cost;
+		}
+		return roomCosts;
+	}
+	
+	// computes the total cost of carpeting for all of the RoomDimension objects combined
+	public double getCarpetCost() {
+		// retrieving carpeting cost for each individual room
+		double [] roomCosts = this.getRoomCosts();
+		// initializing variable to store total cost
+		double totalCost = 0.0;
+		// iterating through Array object containing carpeting costs for each individual RoomDimension object
+		for (int j = 0; j < roomCosts.length; j++) {
+			totalCost += roomCosts[j];
+		}
+		return totalCost;
+	}
 }
--- Lab4/src/Lab4/RoomDimension.java
+++ Lab4/src/Lab4/RoomDimension.java
@@ -1,4 +1,58 @@
 package Lab4;
 
 public class RoomDimension {
+	
+	// ADD MORE TEST CASES
+	
+	private double length;
+	private double width;
+	
+	// general class constructor
+	public RoomDimension(double length, double width) {
+		// validating arguments passed to constructor parameters, assigning 1 for default length and width
+		this.length = (length > 0) ? length : 1;
+		this.width = (width > 0) ? width : 1;
+	}
+	
+	// accessor methods
+	public double getLength() {
+		return this.length;
+	}
+	
+	public double getWidth() {
+		return this.width;
+	}
+	
+	// mutator methods
+	public void setLength(double length) {
+		this.length = (length > 0) ? length : 1;
+	}
+	
+	public void setWidth(double width) {
+		this.width = (width > 0) ? width : 1;
+	}
+	
+	// class method which calculates the area of the RoomDimension object
+	public double getArea() {
+		// unsure of what is best practice here, retrieving attributes directly or through accessor
+		return this.getLength() * this.getWidth();
+	}
+	
+	// class method which represents the values assigned to the RoomDimension object attributes
+	public String toString() {
+		return String.format("Length: %.1f, Width: %.1f", this.getLength(), this.getWidth());
+	}
+	
+	// class method which compares the dimensions of the attributes of 2 RoomDimension objects, to determine if they're equal
+	public boolean equals(RoomDimension other) {
+		return (this.getLength() == other.getLength()) && (this.getWidth() == other.getWidth());
+	}
+	
+	// class method which determines whether a given Fan object is suitable for a given RoomDimension object
+	public boolean isFanSuitable(Fan fan) {
+		// passing area of RoomDimension object as argument since it will be compared with the blowing area of the fan, to determine suitability
+		return fan.isSuitable(this.getArea());
+	}
+	
+	// ANY OTHER TYPES OF HELPER FUNCTIONS TO ADD? (March 11 lecture)
 }
