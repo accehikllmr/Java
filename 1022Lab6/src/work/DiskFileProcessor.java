9ab62e8d410a70b26a3a1ba63b1a3bc113e88443
accehikllmr <135480854+accehikllmr@users.noreply.github.com>
2025-03-27 14:02:51 -0400

    first challenge completed
    learned that even though an exception is an object, a method need not have a return (can be void) and still throw an exception (i.e. throw != return)

--- 1022Lab6/src/work/DiskFileProcessor.java
+++ 1022Lab6/src/work/DiskFileProcessor.java
@@ -10,20 +10,29 @@ public class DiskFileProcessor {
 	
 	public static String[] readFiles (String[] names){
 		
+		// Array object to store output from file, either error message for exception or first line of file
 		String[] lines = new String[names.length];
+		// FileReader object, java implementation
 		FileReader f;
+		// iterating through Array object, with has null values, but whose length is equal to the number of files in names Array object
 		for (int i = 0; i < names.length; i++) {
-			// Reading file from path in local directory
 			try {
+				/* instantiating a new FileReader object, passing path of file and appending String for its name, taken from argument
+				 * if unsuccessful (file not found), throws exception
+				 */
 				f = new FileReader("src/work/" + names[i]);
-				// Creating object as one of ways of taking input
+				// BufferedReader object, java implementation, used to take input from file
 				BufferedReader b = new BufferedReader(f);
+				// trying to read first line of file
 				try {
 					lines[i] = b.readLine();
+					// if successful (no exception thrown) close the file and move on to the next one
 					b.close();
+				// if unsuccessful, catch thrown exception
 				} catch(IOException exception) {
 					lines[i] = "IO Error";
 				}
+			// if first try block throws exception, catch it
 			} catch(FileNotFoundException exception) {
 				lines[i] = "FILE DOESN'T EXIST";
 			}	
