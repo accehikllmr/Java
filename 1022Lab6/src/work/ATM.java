757f719f490ffe9e8706eae98d47f49dc18a4b45
accehikllmr <135480854+accehikllmr@users.noreply.github.com>
2025-03-28 08:36:37 -0400

    added signatures

--- 1022Lab6/src/work/ATM.java
+++ 1022Lab6/src/work/ATM.java
@@ -1,3 +1,13 @@
+///////////////////////////////////////////////////////////////////////////
+//Full Name : Michel Clark
+//Yorku Email : mciehl@my.yorku.ca
+//Date : March 28, 2025
+//Authenticity Declaration:
+//I declare this submission is the result of my own work and has not been
+//shared with any other student or 3rd party content provider. This submitted
+//piece of work is entirely of my own creation.
+///////////////////////////////////////////////////////////////////////////
+
 package work;
 
 import lab6.SavingsAccount;
